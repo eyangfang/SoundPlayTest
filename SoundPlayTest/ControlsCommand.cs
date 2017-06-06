a8b9682616fcc56bc286ec521d83c4c6664ea202
Fang Yang <fang_yang@ctl.creative.com>
2017-06-06 18:01:38 +0800

    Add tow files for MVVM changes

---
+++ SoundPlayTest/ControlsCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SoundPlayTest
+{
+    class ControlsCommand : ICommand
+    {
+        private MainViewModel obj;
+        public ControlsCommand(MainViewModel _obj)
+        {
+            obj = _obj;
+        }
+
+        public bool CanExecute(object p)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object p)
+        {
+
+        }
+    }
+}
---
+++ SoundPlayTest/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace SoundPlayTest
+{
+    class MainViewModel
+    {
+        private MainPage obj = new MainPage();
+
+        private ControlsCommand objCommand;
+
+        public MainViewModel()
+        {
+            objCommand = new ControlsCommand(this);
+        }
+    }
+}
